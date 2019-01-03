5611daee5eba5ebc1ad767a63dbd9999cdd0cc79
13 <1034683568@qq.com>
2019-01-03 18:36:45 +0800

    :tada: web resources Initial commit

--- ssm_dubbo_common/src/main/java/com/ssm/dubbo/common/Constants.java
+++ ssm_dubbo_common/src/main/java/com/ssm/dubbo/common/Constants.java
@@ -10,8 +10,4 @@ public class Constants {
     public static final int RESULT_CODE_SUCCESS = 200;  // 成功处理请求
     public static final int RESULT_CODE_BAD_REQUEST = 412;  // bad request
     public static final int RESULT_CODE_SERVER_ERROR = 500;  // 没有对应结果
-
-    public static final String ARTICLE_CACHE_KEY = "perfect-ssm:article:";//文章key
-    public static final String PICTURE_CACHE_KEY = "perfect-ssm:picture:";//图片key
-
 }
